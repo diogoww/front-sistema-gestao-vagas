dda4d1f04f869b302af83e41799ce7744e0edc0e
Diogo Varaschin <diogovaraschin@outlook.com>
2025-09-05 15:00:44 -0300

    feat: send form data to controller

--- src/main/java/br/com/diogow/FirstPageController.java
+++ src/main/java/br/com/diogow/FirstPageController.java
@@ -3,6 +3,7 @@ package br.com.diogow;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +20,11 @@ public class FirstPageController {
     @GetMapping("/login")
     public String loginCandidate() {
         return "candidate/login";
+    }
+
+    @PostMapping("/create")
+    public String registerCandidate(String candidate_name){
+        System.out.println("nome do candidato: " + candidate_name);
+        return "/candidate/login";
     }
 }

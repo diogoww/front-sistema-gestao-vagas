693b0cee5aff217f64f0238ba13bef1c8f08928c
Diogo Varaschin <diogovaraschin@outlook.com>
2025-09-11 11:19:57 -0300

    fix(auth): accept misspelled 'acess_token' and token/expires aliases

--- src/main/java/br/com/diogow/modules/candidate/dto/Token.java
+++ src/main/java/br/com/diogow/modules/candidate/dto/Token.java
@@ -1,13 +1,18 @@
 package br.com.diogow.modules.candidate.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Token {
 
-    private String access_token;
-    private List<String> roles;
-    private Long expires_in;
+	@JsonAlias({"accessToken", "token", "acess_token"})
+	private String access_token;
+	private List<String> roles;
+	@JsonAlias({"expiresIn"})
+	private Long expires_in;
 }

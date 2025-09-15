77cb19be9e77e1ee926cfbd76140622964c7e4c1
Diogo Varaschin <diogovaraschin@outlook.com>
2025-09-15 11:48:19 -0300

    feat: company register backend

--- src/main/java/br/com/diogow/modules/company/CompanyController.java
+++ src/main/java/br/com/diogow/modules/company/CompanyController.java
@@ -1,18 +1,38 @@
 package br.com.diogow.modules.company;
 
 import br.com.diogow.modules.company.dto.CreateCompanyDTO;
+import br.com.diogow.modules.company.service.CreateCompanyService;
+import br.com.diogow.utils.FormatErrorMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Controller
 @RequestMapping("/company")
 public class CompanyController {
 
+    @Autowired
+    private CreateCompanyService createCompanyService;
+
     @GetMapping("/create")
     public String create(Model model){
         model.addAttribute("company", new CreateCompanyDTO());
+        return "company/create";
+    }
+
+    @PostMapping("/create")
+    public String save(Model model, CreateCompanyDTO createCompanyDTO){
+        try{
+            this.createCompanyService.execute(createCompanyDTO);
+            model.addAttribute("company", new CreateCompanyDTO());
+        } catch (HttpClientErrorException e) {
+            model.addAttribute("error_message", FormatErrorMessage.formatErrorMessage(e.getResponseBodyAsString()));
+            model.addAttribute("company", createCompanyDTO);
+        }
         return "company/create";
     }
 }
---
+++ src/main/java/br/com/diogow/modules/company/service/CreateCompanyService.java
@@ -0,0 +1,23 @@
+package br.com.diogow.modules.company.service;
+
+import br.com.diogow.modules.company.dto.CreateCompanyDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CreateCompanyService {
+
+    public String execute(CreateCompanyDTO createCompanyDTO){
+        RestTemplate rt = new RestTemplate();
+
+        org.springframework.http.HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<CreateCompanyDTO> request = new HttpEntity<>(createCompanyDTO, headers);
+
+        return rt.postForObject("http://localhost:8080/company/", request, String.class);
+    }
+}
